ea6261b21162a3235d41e03ec2bc193edab633a9
Acer <Acer@DESKTOP-50U9SDJ>
2022-10-12 02:28:28 +0530

    commit

---
+++ Assignment-2_Function_Recursion/src/org/function/Factorial.java
@@ -0,0 +1,27 @@
+package org.function;
+
+import java.util.Scanner;
+
+public class Factorial 
+{
+	//fun definition
+	static int factorial(int n)
+	{
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+			
+		}
+		return fact;
+	}
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter noumber which you want to factorial:");
+		int n=s.nextInt();
+		System.out.println(factorial(n));//fun call
+		s.close();
+	}
+
+}
