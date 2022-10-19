5dbf5e65832725759870c34a90e5956a232cb3d7
Acer <Acer@DESKTOP-50U9SDJ>
2022-10-19 13:09:08 +0530

    commit

---
+++ Day-11_CollectionArrayAndLinkedList/src/org/tns/list/ArrayList.java
@@ -0,0 +1,15 @@
+package org.tns.list;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+	ArrayList<Object> list=new ArrayList<>();
+	list.add(11);
+	list.add("Bhagyashri");
+	list.add(89.99f);
+	System.out.println("The elements are: "+list);
+	}
+
+}
