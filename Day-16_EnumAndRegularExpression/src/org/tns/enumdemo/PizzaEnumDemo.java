a9d53cd8d68c7252425d0868ddabf506635ae84f
Acer <Acer@DESKTOP-50U9SDJ>
2022-11-08 20:23:01 +0530

    second commit

--- Day-16_EnumAndRegularExpression/src/org/tns/enumdemo/PizzaEnumDemo.java
+++ Day-16_EnumAndRegularExpression/src/org/tns/enumdemo/PizzaEnumDemo.java
@@ -1,14 +1,9 @@
 package org.tns.enumdemo;
 
-public class PizzaEnumDemo implements InterfacePizzaDemo{
-	SMALL, MEDIUM, LARGE ,EXTRALARGE;
+public class PizzaEnumDemo{
+	enum Size{
+		SMALL,MEDIUM,LARGE,EXTRALARGE
+	};
 
-	@Override
-	public void print() 
-	{
-		System.out.println("Selected size in the pizzas are : "+this);
-		
-		
-	}
 }
 
--- Day-16_EnumAndRegularExpression/src/org/tns/enumdemo/PizzaEnumExecutor.java
+++ Day-16_EnumAndRegularExpression/src/org/tns/enumdemo/PizzaEnumExecutor.java
@@ -2,27 +2,33 @@ package org.tns.enumdemo;
 
 import java.util.Scanner;
 
-public class PizzaEnumExecutor<Size> {
+import org.tns.enumdemo.PizzaEnumDemo.Size;
+
+public class PizzaEnumExecutor{
 	
+	public static void main(String[] args) {
 	Scanner s=new Scanner(System.in);
-	System.out.println()
+	System.out.println("Enter the size of the pizza: ");
 	String size=s.nextLine();
-	Size s1=Size.valueOf(Size.toUpperCase());
-	switch(s1) 
+	//converting String to enum-size of the pizza
+	Size s1=Size.valueOf(size.toUpperCase());
+	switch(s1)
 	{
-	case SMALL;
-		System.out.println("Preparing small pizza for the customer:");
-		break;
-	case MEDIUM;
-	System.out.println("Preparing medium pizza for the customer:");
-	break;
-	case LARGE;
-	System.out.println("Preparing large pizza for the customer:");
-	break;
-	case EXTRALARGE;
-	System.out.println("Preparing extralarge pizza for the customer:");
-	break;
-		
+		case SMALL:
+			System.out.println("Preparing small pizza for the customer");
+			break;
+		case MEDIUM:
+			System.out.println("Preparing Medium pizza for the customer");
+			break;
+		case LARGE:
+			System.out.println("Preparing Large pizza for the customer");
+			break;
+		case EXTRALARGE:
+			System.out.println("Preparing Extra-Large pizza for the customer");
+			break;
+			
 	}
+
 }
-}
+	}
+
